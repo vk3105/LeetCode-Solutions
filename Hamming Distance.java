7ce730a217181ea2aadc7f3567819b8b97d0229c
Vipin Kumar <vipin3105@gmail.com>
2017-03-26 20:06:02 -0400

    Update Hamming Distance.java

--- Hamming Distance.java
+++ Hamming Distance.java
@@ -1,3 +1,6 @@
+/*A simple XOR operation will tell us what we needed for the solution.
+XOR will set the same bits to 0 and different bits to 1, which is what we need to find out.*/
+
 public class Solution {
     public int hammingDistance(int x, int y) {
         
